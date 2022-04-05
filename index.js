72c5b7d62558630941ca66a0f36f04d85ed23bc7
Daven Young <davenyoung@msn.com>
2022-04-05 10:57:55 -0600

    commit tree 0964e854d84902ab73075ab6a36266cd22fcb8ac

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    if (bestCustomer === 'not bob') {
+        bestCustomer = 'maybe bob'
+    }
+}
+
+const leastFavoriteCustomer = 'Daven'
+
+function changeLeastFavoriteCustomer(){
+    if (leastFavoriteCustomer === 'Daven') {
+        leastFavoriteCustomer = 'maybe Daven'
+    }
+}
